2787352562a4856637be7c1166d4cf51979d6e03
KevinHarbour <kharbour22@gmail.com>
2024-02-01 21:37:48 -0500

    M

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById('create-task-form')
+  const taskULElement = document.getElementbyId('tasks')
+  createTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newTaskDescription = document.getElementById('new-task-description')
+    console.log(newTaskDecriptionInpoutElement.value)
+    const liElement = document.createElement('li')
+    liElement.textcontent = newTankDescriptionInputElement.value
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textcontent = 'X'
+
+    deleteButton.addEventListener('click', () =>{
+      deleteButton.parentNode.remove
+    }
+    )
+
+
+    liElement.appendChild(deleteButton)
+    tasksULElement.appendChild(liElement)
 });
+
+
+//const paragraph = document.getElementById('text');
+    //paragraph.textContent = "This is really cool!"
